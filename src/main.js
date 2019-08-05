c045f008a23d5823b30ed7a6fcdc8cf5f5a5e5ac
aispark <aispark@paran.com>
2019-08-05 21:15:54 +0900

    init

--- src/main.js
+++ src/main.js
@@ -5,6 +5,60 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+
+const edge = require('electron-edge-js')
+      
+var helloWorld = edge.func(`
+    async (input) => { 
+        return ".NET Welcomes " + input.ToString(); 
+    }
+`);
+ 
+const add7 = edge.func(`
+    using System.Threading.Tasks;
+ 
+    public class Startup
+    {
+        public async Task<object> Invoke(object input)
+        {
+            int v = (int)input;
+            return Helper.AddSeven(v);
+        }
+    }
+ 
+    static class Helper
+    {
+        public static int AddSeven(int v) 
+        {
+            return v + 7;
+        }
+    }
+`);
+
+const path = require('path')
+
+const projectRootPath = path.resolve(__dirname)
+
+// var add8 = edge.func(require('path').join(__dirname, '../lib/ClassLibrary1.dll'));
+
+helloWorld('JavaScript', function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});  
+
+
+add7(8, (error, result) => {
+    if (error) throw error;
+    console.log(result);
+});  
+add7(8, (error, result) => {
+    if (error) throw error;
+    console.log(result);
+});  
+
+
+    
+
 new Vue({
   router,
   store,
